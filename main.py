9f8d2b766be748bae45851c04aeb501324856a13
Sergey Starodubov <sergeistarshark@mail.ru>
2023-06-07 07:25:20 +0300

    07.06.2023 morning

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-def twoSum(self, nums: list[int], target: int) -> list[int]:
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    indexes = {}  # словарь для хранения значений и соответствующих им индексов
-    for i in range(len(nums)):
-        complement = target_sum - nums[i]
-        if complement in indexes:
-            return [indexes[complement], i]
-        else:
-            indexes[nums[i]] = i
-    return None
+def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = ""
+        count = 0
+        max = 0
+        for i in range(len(s)):
+            if s:
+                if s[i] in res:
+                    count = 1
+                    res = s[i]
+                else:
+                    res = res + s[i]
+                    count += 1
+                
+                if count > max:
+                    max = count
 
-# Пример использования
-my_list = [1, 2, 3, 4, 5, 6]
-target_sum = 9
-result = twoSum(my_list, target_sum=9)
-if result:
-    print(f"Индексы элементов сумма которых равна {target_sum}: {result}")
-else:
-    print(f"Пара элементов сумма которых равна {target_sum} не найдена.")
+            print(f"{res} - {max}")
+
+        return max  
+                  
+
+result = lengthOfLongestSubstring(lengthOfLongestSubstring, "dvdf")
+print(result)
